6b9ef809de72d02ae15448213eca1232d7545009
Chandrasekhar Karri <chandrasekhar@forensicalpha.co>
2024-11-06 10:01:49 +0530

    fastapi

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
-
-
+from pydantic import BaseModel
+from typing import Optional
 app = FastAPI()
 @app.get('/')
 def index():
     return {'hello': {'stu': 'chandu'}}
+@app.get('/blog')
+def blog(limit, published:bool):
+    if published:
+        return {'data': f'{limit} published blogs'}
+    else:
+        return {'data': f'{limit} blogs'}
 
-@app.get('/about')
-def about():
-    return {'about': {'stu': 'chandu'}}
+@app.get('/about/{about_id}')
+def about(about_id:int):
+    return {'about': about_id}
+
+class model(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+    price: float
+    description: Optional[str]=None
+    tax: Optional[float]=None
+
+@app.post('/model/')
+def create_model(request: model):
+    return request
